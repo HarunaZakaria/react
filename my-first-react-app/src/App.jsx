ad1da68f191407418c36894a2617fde9e903b633
Harunzy <Harunzy55@gmail.com>
2025-11-16 10:09:57 +0000

    add new componets

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,14 +1,15 @@
-import './App.css'
+import './App.css';
+import Greeting from './Greeeting';
+import Form from './Form';
 
 function App() {
-
-
   return (
     <>
-      
+      <Greeting />
       <h1>Hello react again</h1>
+      <Form />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
