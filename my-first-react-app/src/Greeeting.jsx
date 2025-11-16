789a4f77cfdf86821767339dae31c48ab70f7a1c
Harunzy <Harunzy55@gmail.com>
2025-11-16 10:11:11 +0000

    add a greeting component

---
+++ my-first-react-app/src/Greeeting.jsx
@@ -0,0 +1,8 @@
+export default function Greet() {
+  return (
+    <div>
+      <h1>&quot; I swear by my pretty floral bonnet, I will end you. &quot;</h1>
+      <p>this is a component that I created</p>
+    </div>
+  );
+}
