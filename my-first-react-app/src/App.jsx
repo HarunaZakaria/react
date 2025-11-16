2fec720d6fe695bfd54087409d066720a8f3c0f3
Harunzy <Harunzy55@gmail.com>
2025-11-16 21:41:10 +0000

    add updates

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,14 +1,24 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import Signup from './Components/Signup';
+import Signin from './Components/Signin';
+import Home from './Components/Home';
 import './App.css';
-import Greeting from './Greeeting';
-import Form from './Form';
 
 function App() {
   return (
-    <>
-      <Greeting />
-      <h1>Hello react again</h1>
-      <Form />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/signup" replace />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
