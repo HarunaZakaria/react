db1027e63f3af5b5ebd82b4656f03592a0c479fb
Harunzy <Harunzy55@gmail.com>
2025-11-17 21:34:07 +0000

    add all

---
+++ my-second-react-app/src/Test.jsx
@@ -0,0 +1,13 @@
+export default function Test() {
+  return (
+    <div>
+      <h1>Testing with ReactJs</h1>
+      <svg>
+        <circle cx={25} cy={75} r={20} stroke="green" strokeWidth={10} />
+      </svg>
+      <form action="#">
+        <input type="text" />
+      </form>
+    </div>
+  );
+}
