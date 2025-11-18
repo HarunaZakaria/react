d7529a636c20a5f6577a08625cb18e2c7a329a07
Harunzy <Harunzy55@gmail.com>
2025-11-18 20:49:16 +0000

    add hooks

---
+++ crush-course/start/fProject/src/ReactHooks.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css';
+// import { use } from 'react';
+//props
+const Card = ({ title }) => {
+  const [count, setCount] = useState(0);
+  const [hasLiked, setHasLiked] = useState('false');
+  useEffect(() => {
+    console.log(`${title} has been ${hasLiked}`);
+  }, [title, hasLiked]);
+
+  return (
+    <div className="card" onClick={() => setCount(count + 1)}>
+      <h2>
+        {title} <br /> {count || null}
+      </h2>
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? '❤️' : '💙'}
+      </button>
+    </div>
+  );
+};
+export default Card;
