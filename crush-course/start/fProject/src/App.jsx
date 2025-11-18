c4c24b5ed1a910e2248a5c59dc9d7f9984dff957
Harunzy <Harunzy55@gmail.com>
2025-11-18 04:17:19 +0000

    add state

--- crush-course/start/fProject/src/App.jsx
+++ crush-course/start/fProject/src/App.jsx
@@ -1,12 +1,18 @@
-// import { useState } from 'react'
+import { useState } from 'react';
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css';
+// import { use } from 'react';
 //props
-const Card = ({title}) => {
+const Card = ({ title }) => {
+  const [hasLiked, setHasLiked] = useState('false');
+
   return (
     <div>
       <h2>{title}</h2>
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? '❤️' : '💙'}
+      </button>
     </div>
   );
 };
@@ -14,9 +20,9 @@ function App() {
   return (
     <div>
       <h1>Arrow component</h1>
-      <Card title='Suit'/>
-      <Card title='How to get away with murder'/>
-      <Card title='Game of thrones'/>
+      <Card title="Suit" />
+      <Card title="How to get away with murder" />
+      <Card title="Game of thrones" />
     </div>
   );
 }
