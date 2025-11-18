6f1b761977ab2d38204ddd93f39120d403e02026
Harunzy <Harunzy55@gmail.com>
2025-11-18 20:47:53 +0000

    add

--- crush-course/start/fProject/src/App.jsx
+++ crush-course/start/fProject/src/App.jsx
@@ -1,27 +1,11 @@
-import { useState, useEffect } from 'react';
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css';
+import Card from './ReactHooks';
+import Tail from './Tail';
 // import { use } from 'react';
 //props
-const Card = ({ title }) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState('false');
-  useEffect(() => {
-    console.log(`${title} has been ${hasLiked}`);
-  }, [title, hasLiked]);
 
-  return (
-    <div className="card" onClick={() => setCount(count + 1)}>
-      <h2>
-        {title} <br /> {count || null}
-      </h2>
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? '❤️' : '💙'}
-      </button>
-    </div>
-  );
-};
 function App() {
   return (
     <div>
@@ -29,6 +13,7 @@ function App() {
       <Card title="Suit" />
       <Card title="How to get away with murder" />
       <Card title="Game of thrones" />
+      <Tail />
     </div>
   );
 }
