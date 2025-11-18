fe5e0172ca5ac5ce5e450cfb199dba893660fea5
Harunzy <Harunzy55@gmail.com>
2025-11-18 20:49:50 +0000

    add

---
+++ crush-course/start/fProject/src/Tail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Tail = () => {
+  return (
+    <div>
+      <h1 class="text-4xl font-bold underline text-gradient">Hello world!</h1>
+    </div>
+  );
+};
+
+export default Tail;
