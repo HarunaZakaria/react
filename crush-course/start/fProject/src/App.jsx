da6895e93c08ed08774c12120f5ee9aceb172965
Harunzy <Harunzy55@gmail.com>
2025-11-18 14:53:31 +0000

    add useeffect

--- crush-course/start/fProject/src/App.jsx
+++ crush-course/start/fProject/src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css';
 // import { use } from 'react';
 //props
 const Card = ({ title }) => {
+  const [count, setCount] = useState(0);
   const [hasLiked, setHasLiked] = useState('false');
+  useEffect(() => {
+    console.log(`${title} has been ${hasLiked}`);
+  }, [title, hasLiked]);
 
   return (
-    <div>
-      <h2>{title}</h2>
+    <div className="card" onClick={() => setCount(count + 1)}>
+      <h2>
+        {title} <br /> {count || null}
+      </h2>
       <button onClick={() => setHasLiked(!hasLiked)}>
         {hasLiked ? '❤️' : '💙'}
       </button>
