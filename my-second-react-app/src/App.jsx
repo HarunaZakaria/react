a592a191597fa5579176720cb7aab8137774c6b3
Harunzy <Harunzy55@gmail.com>
2025-11-19 16:13:20 +0000

    add calculator function

--- my-second-react-app/src/App.jsx
+++ my-second-react-app/src/App.jsx
@@ -3,7 +3,8 @@ import Test from './Test';
 import Image from './ImageTest';
 import List from './Lists';
 import CurrentTime from './CurrentTime';
-import multipleFunc, { add, subtract } from './multipleFunc';
+// import multipleFunc, { add, subtract } from './multipleFunc';
+import * as calculate from './Calculator';
 
 function App() {
   return (
@@ -13,9 +14,16 @@ function App() {
       <List /> */}
       <header>
         {/* <CurrentTime /> */}
-        <p>{multipleFunc()}</p>
+        {/* <p>{multipleFunc()}</p>
         <p>{add()}</p>
-        <p>{subtract()}</p>
+        <p>{subtract()}</p> */}
+
+        <ul>
+          <li>{calculate.add(4, 9)}</li>
+          <li>{calculate.multiply(4, 9)}</li>
+          <li>{calculate.subtract(4, 9)}</li>
+          <li>{calculate.divide(4, 9)}</li>
+        </ul>
       </header>
     </>
   );
---
+++ my-second-react-app/src/Calculator.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+//addtion function
+function add(a, b) {
+  return a + b;
+}
+
+//multiplication function
+function multiply(a, b) {
+  return a * b;
+}
+
+//subtraction function
+function subtract(a, b) {
+  return a - b;
+}
+//division function
+function divide(a, b) {
+  return a / b;
+}
+
+export { add, multiply, subtract, divide };
