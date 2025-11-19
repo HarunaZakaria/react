a11cf251b5d3303c7b317815465b09a887a5b22f
Harunzy <Harunzy55@gmail.com>
2025-11-19 21:19:37 +0000

    add card

--- my-second-react-app/src/App.jsx
+++ my-second-react-app/src/App.jsx
@@ -4,7 +4,11 @@ import Image from './ImageTest';
 import List from './Lists';
 import CurrentTime from './CurrentTime';
 // import multipleFunc, { add, subtract } from './multipleFunc';
-import * as calculate from './Calculator';
+//import * as calculate from './Calculator';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Note from './Components/Note';
+import Card from './Components/Card';
 
 function App() {
   return (
@@ -18,13 +22,33 @@ function App() {
         <p>{add()}</p>
         <p>{subtract()}</p> */}
 
-        <ul>
+        {/* <ul>
           <li>{calculate.add(4, 9)}</li>
           <li>{calculate.multiply(4, 9)}</li>
           <li>{calculate.subtract(4, 9)}</li>
           <li>{calculate.divide(4, 9)}</li>
-        </ul>
+        </ul> */}
+        <Header />
       </header>
+      <main>
+        <Note />
+        {/* Card components here */}
+        <Card
+          name="Haruna"
+          phone="+233545771497"
+          email="Harunzy55@gmail.com"
+          img="./Profile.png"
+        />
+        <Card
+          name="Jalid"
+          phone="+233597929097"
+          email="Jalid55@gmail.com"
+          img="./jalid.jpeg"
+        />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </>
   );
 }
