308486b2cabf016fc7c1904074a01e131c5c9fc0
Harunzy <Harunzy55@gmail.com>
2025-11-20 21:52:33 +0000

    add contacts.jsx

---
+++ my-second-react-app/src/Components/Contacts.jsx
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    id: 1,
+    name: 'Haruna',
+    imgUrl: './Haruna.png',
+    phone: '+233545771497',
+    email: 'Harunzy55@gmail.com',
+  },
+  {
+    id: 2,
+    name: 'Zakaria',
+    imgUrl: './Profile.png',
+    phone: '+233206749221',
+    email: 'zakaria565@gmail.com',
+  },
+  {
+    id: 3,
+    name: 'A. Jalid',
+    imgUrl: './jalid.gpeg',
+    phone: '+233597929097',
+    email: 'jalid515@gmail.com',
+  },
+];
+
+export default contacts;
