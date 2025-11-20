5ae8491fe7b245a2646f52b21d34481aaa0bc6ce
Harunzy <Harunzy55@gmail.com>
2025-11-20 23:29:50 +0000

    add emojipedia.jsx

---
+++ my-second-react-app/src/Components/Emojipedia.jsx
@@ -0,0 +1,109 @@
+const emojipedia = [
+  {
+    id: 1,
+    name: 'Red Heart',
+    emoji: '❤️',
+    meaning:
+      'The red heart ideograph is traditionally used for expressions of love and romance across many cultures, with this being amongst the most frequence use cases for this emoji.',
+  },
+  {
+    id: 2,
+    name: 'Red Heart',
+    emoji: '❤️',
+    meaning:
+      'The red heart ideograph is traditionally used for expressions of love and romance across many cultures, with this being amongst the most frequence use cases for this emoji.',
+  },
+  {
+    id: 3,
+    name: 'Red Heart',
+    emoji: '❤️',
+    meaning:
+      'The red heart ideograph is traditionally used for expressions of love and romance across many cultures, with this being amongst the most frequence use cases for this emoji.',
+  },
+  {
+    id: 4,
+    name: 'Red Heart',
+    emoji: '❤️',
+    meaning:
+      'The red heart ideograph is traditionally used for expressions of love and romance across many cultures, with this being amongst the most frequence use cases for this emoji.',
+  },
+  {
+    id: 5,
+    name: 'Red Heart',
+    emoji: '❤️',
+    meaning:
+      'The red heart ideograph is traditionally used for expressions of love and romance across many cultures, with this being amongst the most frequence use cases for this emoji.',
+  },
+  {
+    id: 6,
+    name: 'Red Heart',
+    emoji: '❤️',
+    meaning:
+      'The red heart ideograph is traditionally used for expressions of love and romance across many cultures, with this being amongst the most frequence use cases for this emoji.',
+  },
+  {
+    id: 7,
+    name: 'Red Heart',
+    emoji: '❤️',
+    meaning:
+      'The red heart ideograph is traditionally used for expressions of love and romance across many cultures, with this being amongst the most frequence use cases for this emoji.',
+  },
+  {
+    id: 8,
+    name: 'Red Heart',
+    emoji: '❤️',
+    meaning:
+      'The red heart ideograph is traditionally used for expressions of love and romance across many cultures, with this being amongst the most frequence use cases for this emoji.',
+  },
+  {
+    id: 9,
+    name: 'Red Heart',
+    emoji: '❤️',
+    meaning:
+      'The red heart ideograph is traditionally used for expressions of love and romance across many cultures, with this being amongst the most frequence use cases for this emoji.',
+  },
+  {
+    id: 10,
+    name: 'Party Popper',
+    emoji: '🎉',
+    meaning:
+      'A party popper, as explodes in a shower of confetti and streamers at a celebration. Generally depicted as a gold party hat, striped on some platforms, at a 45° angle, with multicolored confetti and streamers coming from its opening at the upper right. Twitter’s cone is red.',
+  },
+  {
+    id: 11,
+    name: 'Smiling Face with Smiling Eyes',
+    emoji: '😊',
+    meaning:
+      'A yellow face with smiling eyes and a broad, closed smile turning up to rosy cheeks. Often expresses genuine happiness and warm, positive feelings.',
+  },
+  {
+    id: 12,
+    name: 'Wrapped Gift',
+    emoji: '🎁',
+    meaning:
+      'A present or gift in a box tied with a bow, as given on a birthday or Christmas. Box and ribbon color vary across platforms, but commonly depicted as a gold box with a lid wrapped with a red ribbon.',
+  },
+  {
+    id: 13,
+    name: 'Fire ',
+    emoji: '🔥',
+    meaning:
+      'A flame, as produced when something is on fire. Depicted as a red, orange, and yellow flickering flame.',
+  },
+  {
+    id: 14,
+    name: 'Face with Tears of Joy',
+    emoji: '😂',
+    meaning:
+      'A yellow face with a big grin, uplifted eyebrows, and smiling eyes, each shedding a tear from laughing so hard. Widely used to show something is funny or pleasing. ',
+  },
+  {
+    id: 15,
+    name: 'Loudly Crying Face',
+    emoji: '😭',
+    meaning:
+      'A yellow face with an open mouth wailing and streams of heavy tears flowing from closed eyes. May convey inconsolable grief but also other intense feelings, such as uncontrollable laughter, pride or overwhelming joy.',
+  },
+];
+
+export default emojipedia;
