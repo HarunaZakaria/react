bdfe63bbb2b2a5f8173c5a06f60e3c30edad2824
Harunzy <Harunzy55@gmail.com>
2025-11-24 08:51:25 +0000

    add time.jsx

---
+++ my-second-react-app/src/Components/Time.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+const Time = () => {
+  const CurrentTime = new Date().toLocaleTimeString();
+
+  setInterval(upDateTime,1000)
+  const [time, setTime] = useState(CurrentTime);
+
+  function upDateTime() {
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+
+  return (
+    <div>
+      <h1>{time}</h1>
+      <button onClick={upDateTime}>Get time</button>
+    </div>
+  );
+};
+
+export default Time;
