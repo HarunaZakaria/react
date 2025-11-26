0548364990301a1daefd4f6566abe3914e55cb72
Harunzy <Harunzy55@gmail.com>
2025-11-26 14:29:22 +0000

    add input.jsx

--- my-second-react-app/src/Components/Input.jsx
+++ my-second-react-app/src/Components/Input.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Input = (props) => {
   return (
-    <div>
-      <label htmlFor="#">{props.label}</label>
-      <input type={props.type} placeholder={props.placeholder} required />  
+    <div >
+      <div>
+        <label htmlFor="#">{props.label}</label>
+        <input type={props.type} placeholder={props.placeholder} required />
+      </div>
     </div>
   );
 };
