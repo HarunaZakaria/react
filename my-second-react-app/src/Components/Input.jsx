88eb1d7fe51d15aafa02576b79773bf59dc64fef
Harunzy <Harunzy55@gmail.com>
2025-11-26 20:44:50 +0000

    add bootstrap classes

--- my-second-react-app/src/Components/Input.jsx
+++ my-second-react-app/src/Components/Input.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 
 const Input = (props) => {
   return (
-    <div >
+    <div>
       <div>
-        <label htmlFor="#">{props.label}</label>
-        <input type={props.type} placeholder={props.placeholder} required />
+        <form action="">
+          <label className="form-label" htmlFor="#">
+            {props.label}
+          </label>
+          <input
+            className="form-control"
+            type={props.type}
+            placeholder={props.placeholder}
+            required
+          />
+        </form>
       </div>
     </div>
   );
