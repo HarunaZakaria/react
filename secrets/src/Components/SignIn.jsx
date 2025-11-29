6ec1b04778c191fccd51e545e907ff7f0a849a3c
Harunzy <Harunzy55@gmail.com>
2025-11-29 08:56:47 +0000

    add form to signin.jsx

--- secrets/src/Components/SignIn.jsx
+++ secrets/src/Components/SignIn.jsx
@@ -4,6 +4,35 @@ const SignIn = () => {
   return (
     <div>
       <h1>Sign In</h1>
+      <div className="sign-in-form">
+        <form>
+          <div className="mb-3">
+            <label for="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Your Email Address"
+            />
+          </div>
+          <div className="mb-3">
+            <label for="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Your Password"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
