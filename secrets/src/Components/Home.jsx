a142d4b0696faa9c7554344e10bcc91d62ebd04f
Harunzy <Harunzy55@gmail.com>
2025-11-29 10:44:47 +0000

    add updates

--- secrets/src/Components/Home.jsx
+++ secrets/src/Components/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link, BrowserRouter } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
-      <h1>Elcome Home</h1>
+      <h1>Secrets</h1>
+      <p>Don't keep your Secrets share the anonymously</p>
+      <nav className='home-btns'>
+        <Link to="/SignIn" className='btn btn-primary'>Sign In</Link>
+        <Link to="/Register" className='btn btn-primary'>Register</Link>
+      </nav>
     </div>
   );
 };
