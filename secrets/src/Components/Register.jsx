25f557fb18a8b4258fd8af897a7f016d1b4433e1
Harunzy <Harunzy55@gmail.com>
2025-11-29 09:06:17 +0000

    add a registration form

--- secrets/src/Components/Register.jsx
+++ secrets/src/Components/Register.jsx
@@ -4,6 +4,57 @@ const Register = () => {
   return (
     <div>
       <h1>Register</h1>
+      <div className="register-form">
+        <form>
+          <div className="mb-3">
+            <label for="UserName" className="form-label">
+              User Name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="UserName"
+              placeholder="Your User Name"
+            />
+          </div>
+          <div className="mb-3">
+            <label for="Email" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="Email"
+              placeholder="Your Email Address"
+            />
+          </div>
+          <div className="mb-3">
+            <label for="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Your Password"
+            />
+          </div>
+          <div className="mb-3">
+            <label for="ConfirmPassword" className="form-label">
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="ConfirmPassword"
+              placeholder="Confirm Password"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
