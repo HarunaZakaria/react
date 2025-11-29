9091f1a1585dddf87c9e939e15091bced151bf0b
Harunzy <Harunzy55@gmail.com>
2025-11-29 23:00:55 +0000

    add key icon

--- secrets/src/Components/Home.jsx
+++ secrets/src/Components/Home.jsx
@@ -4,11 +4,18 @@ import { Link, BrowserRouter } from 'react-router-dom';
 const Home = () => {
   return (
     <div>
+      <div>
+        <i className="ph ph-key key"></i>
+      </div>
       <h1>Secrets</h1>
       <p>Don't keep your Secrets share the anonymously</p>
-      <nav className='home-btns'>
-        <Link to="/SignIn" className='btn btn-primary'>Sign In</Link>
-        <Link to="/Register" className='btn btn-primary'>Register</Link>
+      <nav className="home-btns">
+        <Link to="/SignIn" className="btn btn-primary">
+          Sign In
+        </Link>
+        <Link to="/Register" className="btn btn-primary">
+          Register
+        </Link>
       </nav>
     </div>
   );
