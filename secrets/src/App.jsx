e591d6ed4500552868ad80781feb753d4adc70b3
Harunzy <Harunzy55@gmail.com>
2025-11-29 10:45:24 +0000

    add routes

--- secrets/src/App.jsx
+++ secrets/src/App.jsx
@@ -1,9 +1,19 @@
 import './App.css';
+import SignIn from './COmponents/SignIn';
+import Register from './Components/Register';
+import Home from './COmponents/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <h1>Hello App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/SignIn" element={<SignIn />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
