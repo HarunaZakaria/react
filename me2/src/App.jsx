73f200dc4bd403636c2a05d2964a8fbf26858e46
Harunzy <Harunzy55@gmail.com>
2025-12-04 19:15:53 +0000

    add button with event handlers

--- me2/src/App.jsx
+++ me2/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 
 const products = [
@@ -29,18 +30,27 @@ const products = [
 ];
 
 const items = products.map((product) => (
-  <li
-    key={product.id}
-    style={{ color: product.isFruit ? 'magenta' : 'red' }}
-  >
+  <li key={product.id} style={{ color: product.isFruit ? 'magenta' : 'red' }}>
     {product.title}
   </li>
 ));
 
 function App() {
+  const [count, setCount] = useState(0);
+  //handle a button click
+  function Add() {
+    setCount(count + 1);
+  }
+  function Subtract() {
+    setCount(count - 1);
+  }
+
   return (
     <div>
+      <h1>{count}</h1>
       <ul>{items}</ul>
+      <button onClick={Add}>Add</button>
+      <button onClick={Subtract}>Subtract</button>
     </div>
   );
 }
